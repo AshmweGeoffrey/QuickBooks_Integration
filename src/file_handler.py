b0017574c3a4ba52293494a6bf78a6ab65199f45
AshmweGeoffrey <ashimwegeoffrey@gmail.com>
2024-11-18 13:44:48 +0200

    Add: improvements to file_hanlder - improved file_handler - implemented parser for the fetched files using Os Module - Added instruction on how the Script is Going to Work

--- src/file_handler.py
+++ src/file_handler.py
@@ -4,8 +4,16 @@ class file_handler:
     def __init__(self, path):
         self.path = path
         self.files=[]
-
-    def read_paths(self):
-        self.files = os.listdir(self.path)  
+        self.set_paths(self.path)
+    def set_paths(self, path):
+        self.path = path
+        self.files = os.listdir(self.path)
     def get_files(self):
         return self.files
+    def parse_files(self):
+        for file in self.files:
+            if file.endswith(".pdf"):
+                file_details=file.split("_")
+                date=file_details[0]
+                Amount=file_details[1]
+                print("Date: ", date, "Amount: ", Amount)
